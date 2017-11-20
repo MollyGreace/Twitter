aa6e04a51b68a366b19b565382996859735d6b8c
MollyGreace <greace.ss@gmail.com>
2017-11-20 09:20:01 -0500

    modificando archivos

--- js/app.js
+++ js/app.js
@@ -5,12 +5,15 @@ var count = document.getElementById('count');
 
 /*Funciónque imprime en una lista los twit ingresados*/
 btn.addEventListener('click', function(event){
+  area.focus();
+  
   if(area.value) {
+ 
     var li = document.createElement('li');
     var txt = document.createElement('p');
 
     txt.textContent = area.value;
-
+    
     li.appendChild(txt);
     lista.appendChild(li);
 
