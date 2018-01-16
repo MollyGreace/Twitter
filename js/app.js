2aaebb1e32ea87e0f0596acf5e2296f1d225ce56
MollyGreace <greace.ss@gmail.com>
2018-01-16 00:22:28 -0500

    modifica área de posteo

--- js/app.js
+++ js/app.js
@@ -14,34 +14,33 @@ $(document).ready(function() {
     //creamos elementos nuevos
     var post = $(textPost).val();
     var line = '<div class="divider"></div>';
+    var hour = '<div clas="row">' + moment().format('LLL') + '</div>';
     var infoUser = 
       '<div class="row card">' +
         '<div class="col s3 l3">' +
           '<img src="assets/images/user.png" alt="foto del usuario" class="responsive-img">' +
         '</div>' + 
         '<div class="col s9 l9">' +
-          '<div class="row">' +
             '<p class="black-text" id="name">Molly Greace' +
-              '<p class="grey-text text-darken-2" id="name-user">@MollyGreace'+ '</p>' +
+              '<p class="grey-text text-darken-2" id="name-user">@MollyGreace</p>' + hour +
             '</p>' +
-          '</div>';
-    var hour = '<div clas="row">' + moment().format('LLL') + '</div>';
-    var textTwett = '<div class="flow-text">' + post + '</div>';
+          '</div>';   
+    var textTwett = '<p class="row flow-text text-post">' + post + '</p>';
     var buttons = 
       '<div class="row">' +
         '<span class="col s3">' +
           '<i class="material-icons">comment</i>0</span>' +
         '<span class="col s3">' +
-          '<i class="material-icons">share</i>0</span>'+
+          '<i class="material-icons">share</i>0</span>' +
         '<span class="col s3">' +
           '<i class="material-icons">favorite_border</i>0</span>' +
         '<span class="col s3">' +
          '<i class="material-icons">mail_outline</i>0</span>' +
-      '</div></div></div>';
+      '</div></div>';
     
     //creamos la condición
     if ($(textPost).val()) {
-      var containerNewPost = '<div class="section>' + line + infoUser + hour + textTwett + buttons + '</div>';
+      var containerNewPost = '<div class="section>' + line + infoUser + textTwett + buttons + '</div>';
 
       $(containerPost).prepend(containerNewPost);
       $(containerNewPost).before(line);
